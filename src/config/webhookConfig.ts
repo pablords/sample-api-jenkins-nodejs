6345cbff044b1dd27bb899b2656d6e8a8daf819b
pablo <pablords@gmail.com>
2022-03-20 21:31:15 -0300

    *

--- src/config/webhookConfig.ts
+++ src/config/webhookConfig.ts
@@ -1,13 +1,14 @@
 import { Webhooks, createNodeMiddleware } from '@octokit/webhooks'
+import http from "http"
 
 
 const webhooks = new Webhooks({
-    secret: "mysecret",
+    secret: "1q2w3e4r",
 });
 
 webhooks.onAny(({ id, name, payload }) => {
     console.log(name, "event received");
 });
 
-require("http").createServer(createNodeMiddleware(webhooks)).listen(3005);
+http.createServer(createNodeMiddleware(webhooks)).listen(3001);
   // can now receive webhook events at /api/github/webhooks
